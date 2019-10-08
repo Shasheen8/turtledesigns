08c9160b930fc398ce3babbd12d86fe611f07149
Shasheen8 <50681308+Shasheen8@users.noreply.github.com>
2019-10-08 15:56:51 -0400

    Add files via upload

---
+++ design3.py
@@ -0,0 +1,1 @@
+import turtledef spiral (sides,trun, color, width):    t=  turtle.Turtle()    t.color(color)    t.width(width)    t.speed(0)    for n in range(sides):        t.forward(n)        t.right(trun)        t.penup()        t.left(60)        t.forward(10)        t.right(60)        t.pendown()        t.hideturtle()spiral(130,60,"violet",6)
